3611d9abae40fd5f109b47bcb6e306bb3fdc1517
alaqsaka <aqsaabidin15@gmail.com>
2023-07-21 20:24:53 +0700

    install data-table component, add data table for showing list of billboards from a store

--- app/(dashboard)/[storeId]/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/billboards/components/client.tsx
@@ -6,9 +6,11 @@ import { Separator } from "@/components/ui/separator";
 import { Billboard } from "@prisma/client";
 import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
+import { BillboardColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
 
 interface BillboardClientProps {
-  data: Billboard[];
+  data: BillboardColumn[];
 }
 
 export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
@@ -32,6 +34,8 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
       </div>
 
       <Separator />
+
+      <DataTable columns={columns} data={data} />
     </>
   );
 };
--- app/(dashboard)/[storeId]/billboards/page.tsx
+++ app/(dashboard)/[storeId]/billboards/page.tsx
@@ -1,6 +1,7 @@
 import prismadb from "@/lib/prismadb";
-
+import { format } from "date-fns";
 import { BillboardClient } from "./components/client";
+import { BillboardColumn } from "./components/columns";
 
 const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
   const billboards = await prismadb.billboard.findMany({
@@ -12,10 +13,16 @@ const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
     },
   });
 
+  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+    id: item.id,
+    label: item.label,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }));
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={billboards} />
+        <BillboardClient data={formattedBillboards} />
       </div>
     </div>
   );
