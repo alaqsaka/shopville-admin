9a11a7b43fd9755583e6eff4f2585a6f4e839e3e
alaqsaka <aqsaabidin15@gmail.com>
2023-08-01 20:06:35 +0700

    fix api

--- app/api/[storeId]/sizes/[sizeId]/route.ts
+++ app/api/[storeId]/sizes/[sizeId]/route.ts
@@ -40,6 +40,14 @@ export async function PATCH(
       return new NextResponse("Unauthenticated", { status: 401 });
     }
 
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!value) {
+      return new NextResponse("Value is required", { status: 400 });
+    }
+
     const storeByUserId = await prismadb.store.findFirst({
       where: {
         id: params.storeId,
@@ -66,7 +74,10 @@ export async function PATCH(
     });
 
     return NextResponse.json(size);
-  } catch (error) {}
+  } catch (error) {
+    console.log("[SIZE_PATCH]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
 }
 
 export async function DELETE(
@@ -95,7 +106,7 @@ export async function DELETE(
       return new NextResponse("Size Id is required", { status: 400 });
     }
 
-    const size = await prismadb.size.delete({
+    const size = await prismadb.size.deleteMany({
       where: {
         id: params.sizeId,
       },
