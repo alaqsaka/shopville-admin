baa13e173baf270fa5a2019dbac750efdc119081
alaqsaka <aqsaabidin15@gmail.com>
2023-08-06 21:17:17 +0700

    chore: created schema for orders, WIP orders page

---
+++ app/(dashboard)/[storeId]/orders/components/columns.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import CellAction from "./cell-action";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type OrderColumn = {
+  id: string;
+  phone: string;
+  address: string;
+  isPaid: boolean;
+  totalPrice: string;
+  createdAt: string;
+  products: string;
+};
+
+export const columns: ColumnDef<OrderColumn>[] = [
+  {
+    accessorKey: "label",
+    header: "Label",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />,
+    header: "Actions",
+  },
+];
